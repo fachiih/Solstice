e4d42a2d231125aa36f99bb8247f3cc33633ecb7
Facundo Massimilla <fmassimilla@servexternos.santandertecnologia.com.ar>
2019-11-21 16:02:43 -0300

    Caso 01

---
+++ src/Funciones/Buscar.java
@@ -0,0 +1,73 @@
+package Funciones;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class Buscar {
+	
+	public static void Buscador(String Valor) throws Exception {
+		try { 
+			
+			Utilidades.sendKeysXpath("//input[@id='twotabsearchtextbox']", Valor);		
+
+		} catch (Exception e) {
+			
+				throw new Exception();			
+		}
+	}
+	
+	public static void btm_Buscar() throws Exception {
+		try { 
+			
+			Utilidades.clickXpath("//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']");
+
+		} catch (Exception e) {
+			
+				throw new Exception();			
+		}
+	}
+	
+	
+	public static void Swipe() throws Exception {
+		try { 
+			TimeUnit.SECONDS.sleep(2);
+			Utilidades.swipeToXpath("//li[@class='a-normal']//a[contains(text(),'2')]");
+			TimeUnit.SECONDS.sleep(2);
+			Utilidades.clickXpath("//li[@class='a-normal']//a[contains(text(),'2')]");
+			TimeUnit.SECONDS.sleep(2);
+
+		} catch (Exception e) {
+			
+				throw new Exception();			
+		}
+	}
+	
+	public static void TercerItem() throws Exception {
+		try { 
+			TimeUnit.SECONDS.sleep(2);
+			Utilidades.swipeToXpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/span[4]/div[1]/div[3]/div[1]/span[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]");
+			TimeUnit.SECONDS.sleep(2);
+			Utilidades.clickXpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/span[4]/div[1]/div[3]/div[1]/span[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]");
+			TimeUnit.SECONDS.sleep(2);
+
+		} catch (Exception e) {
+			
+				throw new Exception();			
+		}
+	}
+	
+	public static void Validacion() throws Exception {
+		try { 
+			
+			Utilidades.clickXpath("//input[@id='add-to-cart-button']");
+
+		} catch (Exception e) {
+			
+				throw new Exception();			
+		}
+	}
+
+	
+
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,72 @@
+package Main;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Funciones.Buscar;
+import Funciones.Utilidades;
+
+
+public class Main {
+
+	static WebDriver d;
+	
+	public static void main(String[] args) throws Exception {
+		
+		// CHROME DRIVER ALOJADO EN Driver\\
+		System.setProperty("webdriver.chrome.driver","Drivers2\\chromedriver.exe");
+		
+		d = new ChromeDriver();
+		
+		
+     	System.out.println("Generando caso Amazon");
+		
+		// URL DEL APLICATIVO
+		String baseUrl = "http://amazon.com";
+		d.get(baseUrl);
+		d.manage().window().maximize();
+
+		new Utilidades(d);
+		
+	
+		//CORREMOS LAS CASUISTICAS (MODULOS)
+		
+		try {
+			TimeUnit.SECONDS.sleep(15);
+			System.out.println("15 segundos para introducir el Captcha - validacion obligatoria");
+			
+			//Buscamos el articulo
+			Buscar.Buscador("Alexa");
+			System.out.println("Buscamos 'Alexa' en el buscador de amazon");
+			
+			Buscar.btm_Buscar();
+			System.out.println("Tocamos el boton buscar");
+			
+			Buscar.Swipe();
+			System.out.println("Hacemos un Swipe hasta la segunda pagina");
+			
+			Buscar.TercerItem();
+			System.out.println("Seleccionamos el tercer item");
+			
+			Buscar.Validacion();
+			System.out.println("CASO EXITOSO");
+
+			
+		} catch (Exception e) {
+			
+			
+			d.quit();
+			System.err.println("= Error  =");
+			throw new Exception();
+		}
+		d.quit();
+		System.out.println("=== CERRANDO ===");
+		
+		
+		
+		
+	}
+
+}
